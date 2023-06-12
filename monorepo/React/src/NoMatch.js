a9bbb5be029e24b83526271426f5007a50224aef
dardannuredini1998 <136381465+dardannuredini1998@users.noreply.github.com>
2023-06-12 22:29:43 +0200

    added back button to noMatch

--- monorepo/React/src/NoMatch.js
+++ monorepo/React/src/NoMatch.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+
 
 export default function NoMatch() {
     return (
         <div className='list-open'>
           <h1>Not Found 404</h1>
+          <Link className='backButton' to={'/'}>Back to home</Link>
         </div>
     )
 }
